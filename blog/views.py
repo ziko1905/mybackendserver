7433844ff0cd27cb33179a1db1b95b7ec3d5119e
karlo <karlocehlic@gmail.com>
2024-05-07 19:01:37 +0000

    first commit

---
+++ blog/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Posts
+from django.contrib.auth.models import User
+from django.views.generic import (
+    ListView, 
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+# Create your views here.
+
+def home(request):
+    context = {
+        'posts': Posts.objects.all(),
+    }
+    return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Posts
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date']
+    paginate_by = 5
+
+class UserPostListView(ListView):
+    model = Posts
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    ordering = ['-date']
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Posts.objects.filter(author=user).order_by('-date')
+
+class PostDetailView(DetailView):
+    model = Posts
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Posts
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Posts
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Posts
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+
+    
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
